0e625996b6f5801cb9c90333ac1a157924ce3727
arhik <arhik23@gmail.com>
2023-07-24 23:30:06 +0530

    Initial Cartpole example working

---
+++ examples/cartpole3D.jl
@@ -0,0 +1,189 @@
+using DojoGUI
+using Dojo
+using Dojo: input_impulse!, clear_external_force!, update_state!
+using DojoEnvironments
+using AbstractTrees
+using WGPUCore
+using WGPUgfx
+using GLFW
+using GLFW: WindowShouldClose, PollEvents, DestroyWindow
+using ControlSystemsBase
+using LinearAlgebra
+using DataStructures
+using Rotations
+
+
+mechanism = get_mechanism(:rhea)
+mechanism = get_mechanism(:cartpole3D)
+
+
+robot = buildRobot(mechanism)
+
+
+WGPUCore.SetLogLevel(WGPUCore.WGPULogLevel_Off)
+canvas = WGPUCore.defaultCanvas(WGPUCore.WGPUCanvas)
+gpuDevice = WGPUCore.getDefaultDevice()
+
+
+camera = defaultCamera()
+light = defaultLighting()
+grid = defaultGrid()
+axis = defaultAxis()
+
+
+scene = Scene(
+	gpuDevice,
+	canvas,
+	camera,
+	light,
+	[],
+	repeat([nothing], 4)...
+)
+
+
+attachEventSystem(scene)
+
+
+addObject!(scene, grid)
+addObject!(scene, axis)
+addObject!(scene, robot)
+
+
+swapMatrix = [1 0 0 0; 0 0 1 0; 0 -1 0 0; 0 0 0 1] .|> Float32
+
+
+function initTransform!(wn::WorldNode{T}) where T<:Renderable
+	object = wn.object
+	body = wn.body
+	shape = body.shape
+	if !(typeof(shape) <: Dojo.EmptyShape)
+		state = body.state
+		trans = WGPUgfx.translate(state.x1 + Dojo.vector_rotate(shape.position_offset, state.q1))
+		scale = WGPUgfx.scaleTransform(shape.scale)
+		rot = WGPUgfx.rotateTransform(state.q1*shape.orientation_offset)
+		object.uniformData = swapMatrix*(trans∘rot∘scale).linear
+	end
+	for node in wn.childObjs
+		initTransform!(node)
+	end
+end
+
+
+function setTransform!(tNode::WorldNode, t)
+	tNode.object.uniformData = swapMatrix*t
+	if tNode.childObjs == nothing
+		return
+	end
+	# for node in tNode.childObjs
+		# setTransform!(node, t)
+	# end
+end
+
+
+function stepTransform!(wn::WorldNode{T}) where T<:Renderable
+	object = wn.object
+	body = wn.body
+	shape = body.shape
+	if !(typeof(shape) <: Dojo.EmptyShape)
+		state = body.state
+		trans = WGPUgfx.translate(state.x1)
+		scale = WGPUgfx.scaleTransform(shape.scale)
+		rot = WGPUgfx.rotateTransform(state.q1*shape.orientation_offset)
+		setTransform!(wn, (trans∘rot∘scale).linear)
+	end
+	for node in wn.childObjs
+		stepTransform!(node)
+	end
+end
+
+
+function stepController!(mechanism)
+	try
+		controller!(mechanism, 0)
+		for joint in mechanism.joints
+			input_impulse!(joint, mechanism)
+		end
+		status = mehrotra!(mechanism, opts=SolverOptions(verbose=false))
+		for body in mechanism.bodies
+			clear_external_force!(body) 
+		end
+		if status == :failed
+			@error "Solver Status :failed"
+		else
+			for body in mechanism.bodies
+				update_state!(body, mechanism.timestep)
+			end
+		end
+	catch e
+		@info e
+	end
+end
+
+
+bodies = mechanism.bodies
+origin = mechanism.origin
+
+# ### Controller
+stateIdxs = [[4:6..., 10:16...]]
+x0 = zeros(16)
+u0 = zeros(8)
+
+A, B = get_minimal_gradients!(mechanism, x0, u0)
+# Q = [1.0, 1.0, 1.0, 0.2, 0.2, 0.8, 0.002, 0.002, 0.002, 0.002] |> diagm
+# Q = [0.00001, 0.00001, 0.00001, 0.099, 0.099, 0.099, 0.0001, 0.0001, 0.00004, 0.00004] |> diagm
+Q = [0.00001, 0.00001, 0.00001, 0.099, 0.099, 0.099, 0.0001, 0.0001, 0.00004, 0.00004] |> diagm
+
+x_goal = get_minimal_state(mechanism)[stateIdxs...]
+
+actuators = [:left_wheel, :right_wheel]
+
+R = I(length(actuators))
+idxs = [DojoGUI.get_input_idx(mechanism, actuator) for actuator in actuators]
+
+
+function getNode(wn::WorldNode, name::Symbol)
+	body = wn.body
+	bodyName = body.name
+	if bodyName == name
+		return wn
+	end
+	for node in wn.childObjs
+		rNode = getNode(node, name)
+		if rNode != nothing
+			return rNode
+		end
+	end
+end
+
+
+function controller!(mechanism, k)
+	x = get_minimal_state(mechanism)[stateIdxs...]
+	K = lqr(Discrete,A[stateIdxs..., stateIdxs...],B[stateIdxs..., [idxs...]],Q,R)
+    u = K * (x - x_goal)
+    leftWheel = get_joint(mechanism, :left_wheel)
+    rightWheel = get_joint(mechanism, :right_wheel)
+    set_input!(leftWheel, [u[1]])
+    set_input!(rightWheel, [u[2]])
+end
+
+
+function main()
+	camera.eye = [1.0, 0.5, 1.0]
+	initialize!(mechanism, :cartpole3D; body_position=[0.0, 0.0, 0.0])
+	initTransform!(robot)
+	Dojo.initialize_simulation!(mechanism)
+
+	try
+		while !WindowShouldClose(canvas.windowRef[])
+			status = stepController!(mechanism)
+			stepTransform!(robot)
+			runApp(scene)
+			PollEvents()
+		end
+	finally
+		WGPUCore.destroyWindow(canvas)
+	end
+end
+
+
+main()
---
+++ src/DojoGUI.jl
@@ -0,0 +1,245 @@
+module DojoGUI
+
+using WGPUCore
+using WGPUgfx
+using Dojo
+using Dojo: input_impulse!, clear_external_force!, update_state!
+using Dojo: Shape, Node
+using AbstractTrees
+using DojoEnvironments
+using DataStructures
+
+export WorldNode, buildRobot, print_tree, preparePipeline, prepareObject
+
+mutable struct WorldNode{T<:Renderable} <: Renderable
+	parent::Union{Nothing, T}
+	object::Union{Nothing, T}
+	childObjs::Union{Nothing, Vector{T}}
+	body::Union{Nothing, Dojo.Node}
+end
+
+WGPUgfx.isTextureDefined(wo::WorldNode{T}) where T<:Renderable = isTextureDefined(wo.object)
+WGPUgfx.isTextureDefined(::Type{WorldNode{T}}) where T<:Renderable = isTextureDefined(T)
+WGPUgfx.isNormalDefined(wo::WorldNode{T}) where T<:Renderable = isNormalDefined(wo.object)
+WGPUgfx.isNormalDefined(::Type{WorldNode{T}}) where T<:Renderable = isNormalDefined(T)
+
+
+Base.setproperty!(wn::WorldNode{T}, f::Symbol, v) where T<:Renderable = begin
+	(f in fieldnames(wn |> typeof)) ?
+		setfield!(wn, f, v) :
+		setfield!(wn.object, f, v)
+end
+
+Base.getproperty(wn::WorldNode{T}, f::Symbol) where T<:Renderable = begin
+	(f in fieldnames(wn |> typeof)) ?
+		getfield(wn, f) :
+		getfield(wn.object, f)
+end
+
+mutable struct DojoNode{T<:Dojo.Node} <: Dojo.Node{Float64}
+	parent::Union{Nothing, T}
+	object::Union{Nothing, T}
+	childObjs::Union{Nothing, Vector{T}}
+end
+
+AbstractTrees.children(t::WorldNode) = t.childObjs
+
+
+function Base.show(io::IO, mime::MIME{Symbol("text/plain")}, node::WorldNode)
+	summary(io, node)
+	print(io, "\n")
+	renderObj = (node.object != nothing) ? (node.object.renderObj |> typeof) : nothing
+	rType = node.object.rType
+	body = node.body
+	println(io, " rObj : $(renderObj)")
+	println(io, " rType : $(rType)")
+	println(io, " body : $(summary(body)), $(body.id), $(body.name)")
+end
+
+AbstractTrees.children(t::DojoNode) = t.childObjs
+
+function Base.show(io::IO, mime::MIME{Symbol("text/plain")}, node::DojoNode)
+	summary(io, node)
+	parentId = node.parent == nothing ? "NA" : node.parent.object.id
+	objectId = node.object == nothing ? "NA" : node.object.id
+	name = node.object == nothing ? "NA" : "$(node.object.name)"
+	println(io, " parent : $(parentId)")
+	println(io, " object : $(name)")
+	println(io, " id : $(objectId)")
+end
+
+AbstractTrees.printnode(io::IO, node::WorldNode) = Base.show(io, MIME{Symbol("text/plain")}(), node)
+AbstractTrees.printnode(io::IO, node::DojoNode) = Base.show(io, MIME{Symbol("text/plain")}(), node)
+
+
+function setObject!(tNode::WorldNode, obj::Renderable)
+	setfield!(tNode, :object, obj)
+end
+
+function setObject!(tNode::DojoNode, obj::Dojo.Node)
+	setfield!(tNode, :object, obj)
+end
+
+function addChild!(tNode::WorldNode, obj::Union{WorldNode, Renderable})
+	if (tNode.childObjs == nothing)
+		tNode.childObjs = []
+	end
+	if obj in tNode.childObjs
+		return
+	end
+	if typeof(obj) <: WorldNode
+		push!(tNode.childObjs, obj)
+	elseif typeof(obj) <: Renderable
+		push!(tNode.childObjs, WorldNode(tNode, obj, Renderable[]))
+	end
+end
+
+function addChild!(tNode::DojoNode, obj::Union{DojoNode, Dojo.Node})
+	if (tNode.childObjs == nothing)
+		tNode.childObjs = []
+	end
+	if obj.id in [node.object.id for node in tNode.childObjs]
+		return
+	end
+	if typeof(obj) == DojoNode
+		push!(tNode.childObjs, obj)
+	elseif typeof(obj) <: Dojo.Node
+		push!(tNode.childObjs, DojoNode(tNode, obj, Dojo.Node[]))
+	end
+end
+
+function removeChild!(tNode::WorldNode, obj::Renderable)
+	for (idx, node) in enumerate(tNode.childObjs)
+		if obj == node
+			popat!(tNode.childObjs, idx)
+		end
+	end
+end
+
+function removeChild!(tNode::DojoNode, obj::Dojo.Node)
+	for (idx, node) in enumerate(tNode.childObjs)
+		if obj == node
+			popat!(tNode.childObjs, idx)
+		end
+	end
+end
+
+function findTNode(tNode::DojoNode, idx::Int64; recursion=1)
+	if tNode.parent == nothing && length(tNode.childObjs) == 0 && recursion==1
+		return tNode
+	elseif tNode.object.id == idx
+		return tNode
+	else
+		for node in tNode.childObjs
+			nextNode = findTNode(node, idx; recursion=recursion+1)
+			if nextNode ==nothing
+				continue
+			end
+			return nextNode
+		end
+		# return findTNode(tNode.parent, idx; recursion=recursion)
+	end
+	findTNode(tNode.parent, idx; recursion=recursion+1)
+	# @error "Should not have fall through here"
+end
+
+function buildTree(mechanism::Mechanism)
+	rootNode = DojoNode(nothing, nothing, Dojo.Node[])
+	tNode = rootNode
+	for (id, joint) in enumerate(mechanism.joints)
+		@info joint.parent_id, joint.child_id
+		tNode = findTNode(tNode, joint.parent_id)
+		pbody = get_body(mechanism, joint.parent_id)
+		cbody = get_body(mechanism, joint.child_id)
+		tNode.object == nothing && setObject!(tNode, pbody)
+		cbody.parent = pbody
+		addChild!(tNode, cbody)
+	end
+	return rootNode
+end
+
+function materialize!(node::DojoNode)
+	wn = WorldNode(nothing, nothing, Renderable[], nothing)
+	setfield!(wn, :body, getfield(node, :object))
+	shape = getfield(node, :object).shape
+	wo = nothing
+	if typeof(shape) <: Dojo.EmptyShape
+		mesh = defaultWGPUMesh(joinpath(pkgdir(WGPUgfx), "assets", "sphere.obj"); scale=0.01f0, color=[1.0, 0.5, 0.5, 1.0])
+		wo = WorldObject{WGPUMesh}(mesh, RenderType(VISIBLE | SURFACE | BBOX | AXIS), nothing, nothing, nothing, nothing)
+	elseif typeof(shape) <: Dojo.Mesh
+		mesh = WGPUgfx.defaultWGPUMesh(shape.path)
+		wo = WorldObject{WGPUMesh}(mesh, RenderType(VISIBLE | SURFACE | BBOX | AXIS), nothing, nothing, nothing, nothing)
+	end
+	
+	setObject!(wn, wo)
+	
+	for cnode in node.childObjs
+		cn = materialize!(cnode)
+		addChild!(wn, cn)	
+		setfield!(cn, :parent, wn)
+	end
+	
+	return wn
+end
+
+function buildRobot(mechanism::Mechanism)
+	tree = buildTree(mechanism)
+	materialize!(tree)
+end
+
+function get_joint_dims(mechanism::Mechanism)
+	jointNames = mechanism.joints .|> (x) -> getproperty(x, :name)
+	dims = jointNames .|> (x) -> input_dimension(get_joint(mechanism, x))
+	jointDims = OrderedDict{Symbol, Int}(zip(jointNames, dims))
+	return jointDims
+end
+
+function get_input_dims(mechanism::Mechanism, x::Symbol)
+	return get_joint_dims(mechanism)[x]
+end
+
+function get_input_dims(mechanism::Mechanism)
+	return get_joint_dims(mechanism) |> values
+end
+
+function get_input_idx(mechanism::Mechanism, x::Symbol)
+	jointDims = get_joint_dims(mechanism)
+	idx = 0
+	for (k, v) in jointDims
+		idx += v
+		if k == x
+			return idx
+		end
+	end
+	@error "Could not find the symbol"
+end
+
+function WGPUgfx.compileShaders!(gpuDevice, scene::Scene, wn::WorldNode; binding=3)
+	WGPUgfx.compileShaders!(gpuDevice, scene, wn.object; binding=binding)
+	for node in wn.childObjs
+		WGPUgfx.compileShaders!(gpuDevice, scene, node; binding=binding)
+	end
+end
+
+function WGPUgfx.prepareObject(gpuDevice, wn::WorldNode)
+	WGPUgfx.prepareObject(gpuDevice, wn.object)
+	for node in wn.childObjs
+		WGPUgfx.prepareObject(gpuDevice, node)
+	end
+end
+
+function WGPUgfx.preparePipeline(gpuDevice, scene, wn::WorldNode; binding=2)
+	WGPUgfx.preparePipeline(gpuDevice, scene, wn.object; binding=binding)
+	for node in wn.childObjs
+		WGPUgfx.preparePipeline(gpuDevice, scene, node; binding=binding)
+	end
+end
+
+function WGPUgfx.render(renderPass::WGPUCore.GPURenderPassEncoder, renderPassOptions, wn::WorldNode)
+	WGPUgfx.render(renderPass, renderPassOptions, wn.object)
+	for node in wn.childObjs
+		WGPUgfx.render(renderPass, renderPassOptions, node)
+	end
+end
+
+end # module DojoGUI
